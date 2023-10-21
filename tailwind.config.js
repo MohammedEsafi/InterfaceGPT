d7e21891e24f89567171e1fe26ed9265b87f1638
Mohammed Esafi <47392041+MohammedEsafi@users.noreply.github.com>
2023-10-21 19:55:23 +0100

    refactor: tailwind colors config to implement a simplified design system with `brand`, `neutral`, `success`, and `critical` palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -14,61 +13,61 @@ module.exports = {
         sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        base: {
-          background: 'hsl(var(--color-base-background))',
-          foreground: 'hsl(var(--color-base-foreground))',
-        },
-        neutral: {
-          surface: 'hsl(var(--color-neutral-surface))',
-          'on-surface': 'hsl(var(--color-neutral-on-surface))',
-          'surface-secondary': 'hsl(var(--color-neutral-surface-secondary))',
-          'on-surface-secondary': 'hsl(var(--color-neutral-on-surface-secondary))',
-        },
+        // Utilized to represent and emphasize branding elements
         brand: {
-          surface: 'hsl(var(--color-brand-surface))',
-          'on-surface': 'hsl(var(--color-brand-on-surface))',
-          fill: 'hsl(var(--color-brand-fill))',
-          'on-fill': 'hsl(var(--color-brand-on-fill))',
+          50: '#eff5fe',
+          100: '#e2ebfd',
+          200: '#cadafb',
+          300: '#aac0f7',
+          400: '#889ff1',
+          500: '#6b7de9',
+          600: '#4d56db',
+          700: '#4147c1',
+          800: '#373d9c',
+          900: '#33397c',
+          950: '#1e2048',
         },
-        info: {
-          surface: 'hsl(var(--color-info-surface))',
-          'on-surface': 'hsl(var(--color-info-on-surface))',
-          fill: 'hsl(var(--color-info-fill))',
-          'on-fill': 'hsl(var(--color-info-on-fill))',
+        // Employed for setting the background colors and text
+        neutral: {
+          50: '#f6f6f6',
+          100: '#e7e7e7',
+          200: '#d1d1d1',
+          300: '#b0b0b0',
+          400: '#888888',
+          500: '#6d6d6d',
+          600: '#5d5d5d',
+          700: '#4f4f4f',
+          800: '#454545',
+          900: '#3d3d3d',
+          950: '#020202',
         },
+        // Conveys successful actions or positive messages
         success: {
-          surface: 'hsl(var(--color-success-surface))',
-          'on-surface': 'hsl(var(--color-success-on-surface))',
-          fill: 'hsl(var(--color-success-fill))',
-          'on-fill': 'hsl(var(--color-success-on-fill))',
+          50: '#ecfdf5',
+          100: '#d1fae5',
+          200: '#a7f3d0',
+          300: '#6ee7b7',
+          400: '#34d399',
+          500: '#10b981',
+          600: '#059669',
+          700: '#047857',
+          800: '#065f46',
+          900: '#064e3b',
+          950: '#022c22',
         },
-        caution: {
-          surface: 'hsl(var(--color-caution-surface))',
-          'on-surface': 'hsl(var(--color-caution-on-surface))',
-          fill: 'hsl(var(--color-caution-fill))',
-          'on-fill': 'hsl(var(--color-caution-on-fill))',
-        },
-        warning: {
-          surface: 'hsl(var(--color-warning-surface))',
-          'on-surface': 'hsl(var(--color-warning-on-surface))',
-          fill: 'hsl(var(--color-warning-fill))',
-          'on-fill': 'hsl(var(--color-warning-on-fill))',
-        },
+        // Indicates critical, blocked, or error-related messaging
         critical: {
-          surface: 'hsl(var(--color-critical-surface))',
-          'on-surface': 'hsl(var(--color-critical-on-surface))',
-          fill: 'hsl(var(--color-critical-fill))',
-          'on-fill': 'hsl(var(--color-critical-on-fill))',
-        },
-        inverse: {
-          surface: 'hsl(var(--color-inverse-surface))',
-          'on-surface': 'hsl(var(--color-inverse-on-surface))',
-          fill: 'hsl(var(--color-inverse-fill))',
-          'on-fill': 'hsl(var(--color-inverse-on-fill))',
-        },
-        hairline: {
-          surface: 'hsl(var(--color-input-surface))',
-          border: 'hsl(var(--color-input-border))',
+          50: '#fef2f2',
+          100: '#fee2e2',
+          200: '#fecaca',
+          300: '#fca5a5',
+          400: '#f87171',
+          500: '#ef4444',
+          600: '#dc2626',
+          700: '#b91c1c',
+          800: '#991b1b',
+          900: '#7f1d1d',
+          950: '#450a0a',
         },
       },
       keyframes: {
